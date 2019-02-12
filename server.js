564f1a564abababe8c8c9574c34943be1ed500cc
vineeta-chauhan <vineetachauhan69@gmail.com>
2019-02-13 00:25:47 +0530

    add  list and route

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ require('./models/db');
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyparser =require('body-parser');
 
 const employeeController = require('./controllers/employeeController');
 var app = express();
+app.use(bodyparser.urlencoded({
+    ectended:true
+}));
 app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({
     extname: 'hbs',
